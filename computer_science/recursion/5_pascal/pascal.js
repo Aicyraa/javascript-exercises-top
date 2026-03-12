fcdae9f7f695132dc639536ae59c927dc571d8b3
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-03-12 09:08:28 +0800

    ...

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,22 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+   
+   const nextPascal = [] 
+   
+   if (n === 1 ) {
+      return [1]
+   }
+
+   const prevPattern = pascal(n - 1)
+   prevPattern.push(0)
+   prevPattern.unshift(0)
+   
+   for (let i = 0; i < prevPattern.length - 1; i++) {
+      const [consNumA, consNumB] = [prevPattern[i], prevPattern[i + 1]]
+      nextPascal.push(consNumA + consNumB)
+   }
+   
+   return nextPascal
+
 };
   
 // Do not edit below this line
